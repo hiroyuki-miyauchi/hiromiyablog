c5f238dc55b40f4630f50bcb25516aa45928f28f
hiroyuki.miyauchi <hiroyuki.miyauchi0410@gmail.com>
2022-03-21 20:36:21 +0900

    「code」の装飾とGoogleAdSenseのmultiplexのphp追記

--- template-noTemplate.php
+++ template-noTemplate.php
@@ -49,7 +49,7 @@
 <?php endif; ?>
 
 <div class="googleAdSenseHorizontal">
-  <?php get_template_part('template-parts/googleAdSense-horizontal'); // 「GoogleA dSense」横長タイプを読み込む ?>
+  <?php get_template_part('template-parts/googleAdSense-multiplex'); // 「GoogleA dSense」横長タイプを読み込む ?>
 </div><!-- /.googleAdSenseHorizontal -->
 
 <!-- Google AdSense -->
