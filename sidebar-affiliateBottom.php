226d8d1bfabbd72920e18d8c652815fe37ad20bc
hiroyuki.miyauchi <hiroyuki.miyauchi0410@gmail.com>
2022-06-05 18:37:11 +0900

    アフィリエイトバナーの追加実装およびそれに伴うCSS調整

---
+++ sidebar-affiliateBottom.php
@@ -0,0 +1,18 @@
+<aside class="archive">
+  <div class="affiliate">
+    <div class="affiliate__inner">
+      <ul class="affiliate__itemBox">
+        <li class="affiliate__item">
+          <a class="affiliate__link" href="https://px.a8.net/svt/ejp?a8mat=3NCB80+AEHP22+3GWO+6C9LD" rel="nofollow">
+          <img class="affiliate__image" border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=220604112629&wid=002&eno=01&mid=s00000016188001065000&mc=1"></a>
+          <img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3NCB80+AEHP22+3GWO+6C9LD" alt="">
+        </li>
+        <li class="affiliate__item">
+          <a class="affiliate__link" href="https://px.a8.net/svt/ejp?a8mat=3NCB80+59XBIY+2PEO+ONRVL" rel="nofollow">
+          <img class="affiliate__image" border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=220604112319&wid=002&eno=01&mid=s00000012624004142000&mc=1"></a>
+          <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3NCB80+59XBIY+2PEO+ONRVL" alt="">
+        </li>
+      </ul>
+    </div><!-- /.affiliate__inner -->
+  </div><!-- /.affiliate -->
+</aside>
